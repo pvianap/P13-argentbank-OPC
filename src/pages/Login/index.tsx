9fc38baba8881dadf9c0b13eef30712e0f9934ab
pvianap <pedro.v.parente@gmail.com>
2022-12-30 11:22:52 +0100

    login page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -3,5 +3,29 @@ import React from 'react';
 type Props = {};
 
 export default function Login({}: Props) {
-  return <div>Login</div>;
+  return (
+    <div>
+      <section className="sign-in-content">
+        <i className="fa fa-user-circle sign-in-icon"></i>
+        <h1>Sign In</h1>
+        <form>
+          <div className="input-wrapper">
+            <label htmlFor="username">Username</label>
+            <input type="text" id="username" />
+          </div>
+          <div className="input-wrapper">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" />
+          </div>
+          <div className="input-remember">
+            <input type="checkbox" id="remember-me" />
+            <label htmlFor="remember-me">Remember me</label>
+          </div>
+
+          {/* SHOULD BE THE BUTTON BELOW  */}
+          <button className="sign-in-button">Sign In</button>
+        </form>
+      </section>
+    </div>
+  );
 }
